5f6583533b76434455e848275c32e78c3cb494c0
Maguilar <Maguilar@github.equilend.com>
2023-08-09 15:14:21 +0000

    Get max attempts from configurator

--- src/main/java/com/equilend/simulator/Configurator.java
+++ src/main/java/com/equilend/simulator/Configurator.java
@@ -12,9 +12,11 @@ public class Configurator {
     //Configurator should probably look for config file itself..
     private static String lenderFilename = "src/main/java/com/equilend/simulator/config/lender_config.txt";
     private static String borrowerFilename = "src/main/java/com/equilend/simulator/config/borrower_config.txt";
+    //max attempts should be read from config file
+    private static int maxAttempts = 3;
 
     // TODO: Validate the login info file instead of blindly creating hash map
-    public static Map<String, String> readLoginConfig (String filename) 
+    private static Map<String, String> readLoginConfig (String filename) 
     {
         Map<String, String> loginInfo = new HashMap<>();
         try (Scanner scanner = new Scanner(new File(filename))){
@@ -38,5 +40,9 @@ public class Configurator {
     public static User createBorrower(){
         User borrower = new User(readLoginConfig(borrowerFilename), PartyRole.BORROWER);
         return borrower;
+    }
+
+    public static int getMaxAttempts(){
+        return maxAttempts;
     }
 }
--- src/main/java/com/equilend/simulator/Simulator.java
+++ src/main/java/com/equilend/simulator/Simulator.java
@@ -7,18 +7,17 @@ import java.time.OffsetDateTime;
 public class Simulator 
 {   
     private static String allParties = "*";
-    private static final int MAX_ATTEMPTS = 3;
     public static void main(String[] args)  
     {
-        
         User lender = Configurator.createLender();
         assert(lender.isValid()); 
         User borrower = Configurator.createBorrower();
         assert(borrower.isValid());
         
+        int attempts = 0;
+        final int MAX_ATTEMPTS = Configurator.getMaxAttempts();
         OffsetDateTime since = APIConnector.getCurrentTime();
         OffsetDateTime before;
-        int attempts = 0;
         while (true){
             waitMillisecs(5000L);
             before = APIConnector.getCurrentTime();
@@ -28,18 +27,16 @@ public class Simulator
                 //well, aCP() checks for contracts in the proposed state before sending approve request.
                 //what about pCFA() tho? 
                 //WELL... trade agreements are supposed to eventually be consumed once a contract is proposed from it. this doesn't happen w my generated shtts tho..
-
                 attempts++;
                 lender.refreshToken();
                 borrower.refreshToken();
             }
             if (attempts == MAX_ATTEMPTS){
-                System.out.println("Not working mate. Something must be up");
+                System.out.println(String.format("We've tried %d times mate its JUST NOT WORKING", MAX_ATTEMPTS));
                 return;
             }
             attempts = 0;
             since = before;
-
         }
     }
     
