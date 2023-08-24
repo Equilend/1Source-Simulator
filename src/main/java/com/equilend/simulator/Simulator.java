2a8816d02faa6eb7c90910a0106f093b8568570b
Matt <Maguilar@github.equilend.com>
2023-08-24 16:40:23 +0000

    Enter q in console to quit

--- src/main/java/com/equilend/simulator/Simulator.java
+++ src/main/java/com/equilend/simulator/Simulator.java
@@ -1,8 +1,8 @@
 package com.equilend.simulator;
 
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,13 +18,27 @@ public class Simulator
     public static void main(String[] args)  
     {
         Configurator configurator = new Configurator();
+        
         //use configurator to set up Token class..
         BearerToken.configureToken(configurator.getLoginMap());
-
+        
         ExecutorService exec = Executors.newSingleThreadExecutor();
         exec.execute(new EventsProcessor(configurator));
+        
+        System.out.println("Enter q to quit");
+        Scanner input = new Scanner(System.in);
+        String i;
+        while (input.hasNext()){
+            i = input.nextLine();
+            if (i.equalsIgnoreCase("q")){
+                System.out.println("You've pressed \'q\'");
+                System.out.println("Let us clean up a lil and we'll be done in 5 secs at MOST...");
+                exec.shutdownNow();
+                break;
+            }
+        }
+        input.close();
 
-        exec.shutdown();
         logger.info("DONE :)");
  
     }
